a44a53ba79cf5364c505a750d65227dc6ec9b23d
karuiner <intaekgong@gmail.com>
2022-06-25 23:31:17 +0900

    로그인 기능 부여

--- mini-chat/server/index.ts
+++ mini-chat/server/index.ts
@@ -3,12 +3,21 @@ import { Server } from "socket.io";
 const io = new Server(4000, {
   /* options */
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "*"],
   },
 });
+let unumber = 0;
+let db = {};
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  let userName = `Guest-${`${unumber}`.padStart(8, "0")}`;
+  socket.emit("Login", { userName: userName });
+  unumber++;
+  // socket.on("Login", () => {
+  //   socket.emit("Login", { userName: userName });
+  //   unumber++;
+  // });
+
   socket.on("test", (req) => {
     console.log(req);
   });
