629190d1058a864f8a68be8f23d2e77e6a9327f4
karuiner <intaekgong@gmail.com>
2022-06-26 20:46:38 +0900

    초기 정보 전달 및 이름 변경 구현

--- mini-chat/server/index.ts
+++ mini-chat/server/index.ts
@@ -7,12 +7,43 @@ const io = new Server(4000, {
   },
 });
 let unumber = 0;
-let db = {};
+interface user {
+  userName: string;
+  roomName: string;
+}
+interface data {
+  [key: string]: user;
+}
+
+let db: data = {},
+  users: { [key: string]: boolean } = {};
 
 io.on("connection", (socket) => {
   let userName = `Guest-${`${unumber}`.padStart(8, "0")}`;
-  socket.emit("Login", { userName: userName });
+  socket.data.userName = userName;
+  socket.data.roomName = "로비";
+  users[userName] = true;
+  socket.emit("Login", { userName: userName, users: Object.keys(users) });
+  socket.join("로비");
+  socket
+    .to("로비")
+    .emit("Ologin", { userName: userName, users: Object.keys(users) });
   unumber++;
+
+  socket.on("Name-Change", (req) => {
+    socket.to(req.roomName).emit("Orther-Name-Change", {
+      oldName: socket.data.userName,
+      newName: req.userName,
+    });
+  });
+
+  // socket.on("Room-Name-Change", (req) => {
+  //   socket.to(req.roomName).emit("Orther-Name-Change", {
+  //     oldName: socket.data.userName,
+  //     newName: req.userName,
+  //   });
+  // });
+
   // socket.on("Login", () => {
   //   socket.emit("Login", { userName: userName });
   //   unumber++;
