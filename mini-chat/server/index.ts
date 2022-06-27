ca4821b44956055745c3cf7d173fa507f62bbb56
karuiner <intaekgong@gmail.com>
2022-06-27 22:12:48 +0900

    대화 방에 관련된 서버 기능 구현. 방 퇴장은 고민중

--- mini-chat/server/index.ts
+++ mini-chat/server/index.ts
@@ -16,6 +16,8 @@ interface data {
 }
 
 let db: data = {},
+  rooms = { 로비: "room00000000" },
+  roomId = 1,
   users: { [key: string]: boolean } = {};
 
 io.on("connection", (socket) => {
@@ -24,9 +26,9 @@ io.on("connection", (socket) => {
   socket.data.roomName = "로비";
   users[userName] = true;
   socket.emit("Login", { userName: userName, users: Object.keys(users) });
-  socket.join("로비");
+  socket.join(rooms["로비"]);
   socket
-    .to("로비")
+    .to(rooms["로비"])
     .emit("Ologin", { userName: userName, users: Object.keys(users) });
   unumber++;
 
@@ -34,6 +36,52 @@ io.on("connection", (socket) => {
     socket.to(req.roomName).emit("Orther-Name-Change", {
       oldName: socket.data.userName,
       newName: req.userName,
+    });
+  });
+
+  // 새방 만들기
+  socket.on("new-room", (req) => {
+    if (rooms[req.roomName] === undefined) {
+      rooms[req.roomName] = `room${roomId}`.padStart(8, "0");
+      roomId++;
+      socket.to(rooms[req.roomName]).emit("new-room", {
+        roomName: req.roomName,
+      });
+      socket.emit("new-room", {
+        roomName: req.roomName,
+      });
+    } else {
+      socket.emit("used-roomName", "사용 된 방이름");
+    }
+  });
+
+  // 방 입장
+  socket.on("join-room", (req) => {
+    socket.data.roomName = req.roomName;
+    socket.join(rooms[req.roomName]);
+    socket.to(rooms[req.roomName]).emit("join-room", {
+      userName: socket.data.userName,
+    });
+    socket.emit("join-room", {
+      userName: socket.data.userName,
+    });
+  });
+
+  // 방 퇴장 - 좀더 확인해볼 것
+  socket.on("room-out", (req) => {});
+
+  // 방 이름 변경
+  socket.on("room-name-change", (req) => {
+    let roomIh = rooms[req.oldRoomName];
+    delete rooms[req.oldRoomName];
+    rooms[req.newRoomName] = roomIh;
+    socket.to(req.roomName).emit("room-name-change", {
+      oldName: req.oldRoomName,
+      newName: req.newRoomName,
+    });
+    socket.emit("room-name-change", {
+      oldName: req.oldRoomName,
+      newName: req.newRoomName,
     });
   });
 
